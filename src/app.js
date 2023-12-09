d8d5031b632b11b25d040c2601167a6c187f9bf3
shahriarabir1 <shahriarabirjob@gmail.com>
2023-12-09 16:27:40 +0600

    userController

--- src/app.js
+++ src/app.js
@@ -15,4 +15,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser);
+
+import userRouter from "./routers/user.routes.js";
+app.use("/api/v1/users", userRouter);
+
 export { app };
